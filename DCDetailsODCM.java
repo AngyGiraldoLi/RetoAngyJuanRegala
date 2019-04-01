2361edca0740453244101350eff248f07a725312
AngyGiraldoLi <46436340+AngyGiraldoLi@users.noreply.github.com>
2019-04-01 11:38:04 -0500

    Update DCDetailsODCM.java

--- DCDetailsODCM.java
+++ DCDetailsODCM.java
@@ -1,4 +1,4 @@
-package co.com.bancolombia.certification.finaclefe.userinterface.odcm;
+package co..odcm;
 
 import org.openqa.selenium.By;
 
@@ -10,7 +10,7 @@ public class DCDetailsODCM extends PageObject {
 	public static final Target APPLICABLE_RULES = Target.the("Applicable Rules")
 			.located(By.name("odcmdcdetails.applicableRules"));
 	public static final Target TXTPERIODDESC = Target.the("TXT Periodo Presentation").located(By.id("periodDesc"));
-	public static final Target BTNACCEPT = Target.the("Botón Aceptar").located(By.id("Accept"));
+	public static final Target BTNACCEPT = Target.the("BotÃ³n Aceptar").located(By.id("Accept"));
 	public static final Target LINKPERIODPRESENTATION = Target.the("Periodo Presentation").located(By.id("sLnk23"));
 	public static final Target DATEOFEXPIRY = Target.the("Date of Expiry").located(By.id("expDate_ui"));
 	public static final Target NEXTPAGE = Target.the("Next Page").located(By.id("NextPage"));
@@ -22,8 +22,8 @@ public class DCDetailsODCM extends PageObject {
 	public static final Target PLACEOFEXPIRY = Target.the("Place of Expiry")
 			.located(By.name("odcmdcdetails.plExpDate"));
 	public static final Target DC_DETAILS = Target.the("dcdetails").located(By.id("odcmdcdetails"));
-	public static final Target LINKCHARGESDESCRIPCION = Target.the("charges Descripción").located(By.id("sLnk41"));
-	public static final Target TEXTCHARGESDESCRIPCION = Target.the("Text charges Descripción").located(By.id("chargesTextDesc"));
+	public static final Target LINKCHARGESDESCRIPCION = Target.the("charges DescripciÃ³n").located(By.id("sLnk41"));
+	public static final Target TEXTCHARGESDESCRIPCION = Target.the("Text charges DescripciÃ³n").located(By.id("chargesTextDesc"));
 	public static final Target LINKSENDERTORECEIVER = Target.the("Sender to Receiver").located(By.id("sLnk45"));
 	public static final Target TEXTKSENDERTORECEIVER = Target.the("Text Sender to Receiver").located(By.id("senToRecDesc"));
 	public static final Target TENOR = Target.the("Tenor").located(By.id("tenor"));
